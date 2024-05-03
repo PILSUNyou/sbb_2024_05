3bf5bf53e504713ae85df92c1f7eac65ed7a200c
PILSUNyou <dbyo1125@naver.com>
2024-05-03 16:22:07 +0900

    ddl-auto update 변경

--- src/main/java/com/sbs/sbb/Question.java
+++ src/main/java/com/sbs/sbb/Question.java
@@ -26,7 +26,7 @@ public class Question {
     // maooedBy Answer 클래스의 question 변수 이름을 적어야함.(Answer 클래스에 있는 question이랑 이름이 같아야함.)
     // CascadeType.REMOVE를 하면 Question을 삭제할 때 답변도 함께 삭제한다.
     // OneToMany는 테이블의 컬럼으로 생성되지 않는다.
-    // 선택적으로 작성
+    // OneToMany는 선택적으로 작성한다.
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
 }
