00d8394d273a65ac94b67ff9e9645afb5ca753b9
PILSUNyou <dbyo1125@naver.com>
2024-05-08 17:22:45 +0900

    exception 추가, detail 수정

--- src/main/java/com/sbs/sbb/question/QuestionService.java
+++ src/main/java/com/sbs/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.sbs.sbb.question;
 
+import com.sbs.sbb.DataNotException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +14,16 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> oq = this.questionRepository.findById(id);
+        // !oq.isPresent()
+        // oq.isPresent() == false
+        // oq.isEmpty()
+        // 세 가지중 하나 사용
+        if ( oq.isEmpty() ) throw new DataNotException("question not found !!");
+
+        return oq.get();
     }
 }
