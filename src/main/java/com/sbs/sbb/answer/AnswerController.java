48c0c7efd9d73ac449814a2ae4d1c44f17b178c9
PILSUNyou <dbyo1125@naver.com>
2024-05-09 14:29:27 +0900

    답변 저장 기능

--- src/main/java/com/sbs/sbb/answer/AnswerController.java
+++ src/main/java/com/sbs/sbb/answer/AnswerController.java
@@ -15,11 +15,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content){
         // 답변 부모 질문 객체를 받아온다.
         Question q = this.questionService.getQuestion(id);
+
+        // TODO: 답변 객체를 만든다.
+        // TODO는 해야할 일을 써넣는 주석이다.
+
+        Answer answer = this.answerService.create(q, content);
 
         return "redirect:/question/detail/%d".formatted(id);
     }
