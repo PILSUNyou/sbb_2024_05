47e1e4f5301db518761c7266842b83375a4a0800
PILSUNyou <dbyo1125@naver.com>
2024-05-13 16:38:33 +0900

    회원 엔티티, 회원 리포지터리, 회원 서비스 생성

---
+++ src/main/java/com/sbs/sbb/user/UserRepository.java
@@ -0,0 +1,16 @@
+package com.sbs.sbb.user;
+
+import com.sbs.sbb.answer.Answer;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<SiteUser, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(value="ALTER TABLE site_user AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
+
+}
