b1f49fa75c98156dfa58e6f7b74526a157c708ad
PILSUNyou <dbyo1125@naver.com>
2024-05-13 18:11:14 +0900

    로그인 기능 구현

--- src/main/java/com/sbs/sbb/user/UserRepository.java
+++ src/main/java/com/sbs/sbb/user/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Integer> {
 
     @Transactional
@@ -13,4 +15,5 @@ public interface UserRepository extends JpaRepository<SiteUser, Integer> {
     @Query(value="ALTER TABLE site_user AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
 
+    Optional<SiteUser> findByusername(String username);
 }
